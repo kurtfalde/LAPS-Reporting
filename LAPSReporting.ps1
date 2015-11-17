e171d849dc58ab12841c66dc3b6d30129015d2f3
Kurt Falde <itfalde@hotmail.com>
2015-11-17 09:17:23 -0500

    LAPS-Reporting Setup
    LAPS-Reporting Setup

---
+++ LAPSReporting.ps1
@@ -0,0 +1,94 @@
+﻿<#
+    This Sample Code is provided for the purpose of illustration only and is not 
+    intended to be used in a production environment.  THIS SAMPLE CODE AND ANY 
+    RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER 
+    EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF 
+    MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.  We grant You a 
+    nonexclusive, royalty-free right to use and modify the Sample Code and to 
+    reproduce and distribute the object code form of the Sample Code, provided 
+    that You agree: 
+    (i)            to not use Our name, logo, or trademarks to market Your 
+                    software product in which the Sample Code is embedded; 
+    (ii)           to include a valid copyright notice on Your software product 
+                    in which the Sample Code is embedded; and 
+    (iii)          to indemnify, hold harmless, and defend Us and Our suppliers 
+                    from and against any claims or lawsuits, including attorneys’ 
+                    fees, that arise or result from the use or distribution of 
+                    the Sample Code.
+
+
+    Originally written by Kurt Falde Using PSEventLogWatcher from http://pseventlogwatcher.codeplex.com/ 
+    New version for parsing LAPS Audit Log entries 9/20/2015 Kurt Falde MSFT Services
+
+#>
+
+
+
+$workingdir = "c:\laps"
+
+cd $workingdir
+
+#The following line was needed on a 2008 R2 setup at one customer It shouldn't hurt having it here however this can be removed in some cases
+Add-Type -AssemblyName System.Core
+
+#Following added to unregister any existing SLAMWatcher Event in case script has already been ran
+Unregister-Event LAPSWatcher -ErrorAction SilentlyContinue
+Remove-Job LAPSWatcher -ErrorAction SilentlyContinue
+
+
+Import-Module .\EventLogWatcher.psm1
+
+$BookmarkToStartFrom = Get-BookmarkToStartFrom
+
+# The type of events that you want to parse.. in most cases we are parsing the Forwarded Events log with a specific xpath query for the events we want.
+
+$XpathQuery = "*[System[EventID=4662]] and *[EventData[Data[@Name='ObjectType'] and (Data='%{bf967a86-0de6-11d0-a285-00aa003049e2}')]]"
+$EventLogQuery = New-EventLogQuery "ForwardedEvents" -Query $XpathQuery
+
+
+$EventLogWatcher = New-EventLogWatcher $EventLogQuery $BookmarkToStartFrom 
+
+$action = {     
+                
+                #Following is debug line while developing as it will let you enter the nested prompt/session where you
+                #can actually query the $EventRecord / $EventRecordXML etc for troubleshooting
+                #$host.EnterNestedPrompt()
+                $outfile = "c:\laps\laps.csv"
+    
+
+                $EventObj = New-Object psobject
+                $EventObj | Add-Member noteproperty DomainController $EventRecord.MachineName
+                $EventObj | Add-Member noteproperty User $EventRecordXml.SelectSingleNode("//*[@Name='SubjectUserName']")."#text"
+                
+                #Adding Date in Short Format to .csv object (Time is nice but it gets very messy when you start putting in Excel/PowerBI to aggregate down to hours/days etc)
+                $EventObj | Add-Member noteproperty EventDate $EventRecord.TimeCreated.ToShortDateString()
+                
+                #Adding EventRecordID field to .csv object for a unique identifier
+                $EventObj | Add-Member noteproperty EventRecordID $EventRecordXML.Event.System.EventRecordID
+                
+                #Adding Target Computer Object after resolving GUID to name
+                $ComputerGUID = ($EventRecordXML.SelectSingleNode("//*[@Name='ObjectName']")."#text") -replace "%", "" -replace "{", "" -replace "}", ""
+                $ComputerName = (get-adobject -id $ComputerGUID).Name
+                $EventObj | Add-Member noteproperty TargetComputer $ComputerName
+                
+                          
+           If ($Outfile -ne $Null)
+            {
+                write-host $EventObj
+                $EventObj | Convertto-CSV -Outvariable OutData -NoTypeInformation 
+                
+                $OutPath = $Outfile
+                If (Test-Path $OutPath)
+                {
+                    $Outdata[1..($Outdata.count - 1)] | ForEach-Object {Out-File -InputObject $_ $OutPath -append default}
+                } else {
+                    Out-File -InputObject $Outdata $OutPath -Encoding default
+                }
+            }
+            
+            
+            }
+
+Register-EventRecordWrittenEvent $EventLogWatcher -action $action -SourceIdentifier LAPSWatcher
+
+$EventLogWatcher.Enabled = $True
---
+++ LAPSServerSetup.ps1
@@ -0,0 +1,101 @@
+﻿<#  Setup script for Subscription Server for LAPS Audit Events and reporting
+
+Microsoft Services - Kurt Falde
+
+#>
+
+#Please change the $DCTOQUERY value to the DC that has been used for Audit Testing
+$DCTOQUERY = "ADDC"
+cd C:\LAPS
+
+If ((Get-WindowsFeature -Name RSAT-AD-PowerShell).Installed -eq $false){
+    write-host "This needs the AD Powershell commandlets to proceed please run Install-WindowsFeature -Name RSAT-AD_PowerShell"
+    Exit
+    }
+
+$rootdse = Get-ADRootDSE
+$Guids = Get-ADObject -SearchBase ($rootdse.SchemaNamingContext) -LDAPFilter "(schemaidguid=*)" -Properties lDAPDisplayName,schemaIDGUID
+ForEach ($Guid in $Guids){
+  If ($guid.lDAPDisplayName -Like "*ms-mcs-admpwd"){
+  $SGuid = ([System.GUID]$guid.SchemaIDGuid).Guid
+  Write-host -ForegroundColor Cyan "The SchemaIDGuide for ms-mcs-admpwd in this forest is"([System.GUID]$guid.SchemaIDGuid)
+  }
+ }
+
+$xpath =  "*[EventData[Data[@Name='AccessMask'] and (Data='0x100')]] and *[System[EventID=4662]] and *[EventData[Data[@Name='ObjectType'] and (Data='%{bf967a86-0de6-11d0-a285-00aa003049e2}')]]"
+$LAPSAuditEvents = Get-WinEvent -ComputerName $DCTOQUERY -LogName Security -FilterXPath "$xpath" -MaxEvents 200
+
+$xpathdata = @()
+:Outer Foreach($LAPSAuditevent in $LAPSAuditEvents){
+    [xml]$LAPSAuditEventXML = $LAPSAuditEvent.ToXml()
+    $EventXMLNames = $LAPSAuditEventXML.Event.EventData.Data
+
+    #loop through the "Properties" XML Node looking for Events that contain the LAPS ms-mscadmpwd SchemaIDGuid
+    #Add all hits to a new Array $xpathdata
+
+        Foreach ($EventXMLName in $EventXMLNames){
+            If (($EventXMLName.name -eq "Properties") -and ($EventXMLName.'#text' -match "$SGuid")){
+                $LAPSProp = $EventXMLName.'#text'
+                If ($xpathdata.count -eq 0){$xpathdata += $LAPSProp}
+                ElseIf (($LAPSProp | Select-String -AllMatches $xpathdata) -eq $null){$xpathdata += $LAPSProp   }
+                }
+        }
+}
+
+#Parsing through the $xpathdata array to create the right text needed for the xpath filter
+$xpathguid = $null
+If ($xpathdata.Count -le 1) {
+    Write-host -ForegroundColor Cyan "Either a Single or No Xpath Filters created are you sure you tested LAPS UI and using ADUC at a minium on the DC you are testing?"
+    $xpathguid = "(Data='$xpathdata')" 
+    }
+
+ElseIf ($xpathdata.Count -gt 1) {
+    Write-host -ForegroundColor Green "More than a single xpath filter found creating full xpath query output"
+    Foreach ($xpaththing in $xpathdata){
+        If ($xpaththing -ne $xpathdata[-1]){
+        $xpathguid += "(Data='$xpaththing') or "
+        }
+        Else{$xpathguid += "(Data='$xpaththing')"}
+        }
+      }
+ 
+#FYI by default we are filtering out local queries to this from the "SYSTEM" account on a DC as this appears to happen with a DC reading it's own account in AD and creates noise
+#$xpathlaps = "<QueryList><Query Id=""0"" Path=""Security""><Select Path=""Security"">*[System[EventID=4662]] and *[EventData[Data[@Name='SubjectUserSid'] !='S-1-5-18']] and *[EventData[Data[@Name='ObjectType'] and (Data='%{bf967a86-0de6-11d0-a285-00aa003049e2}')]] and *[EventData[Data[@Name='Properties']]]</Select></Query></QueryList>"
+#$xpathlaps = $xpathlaps.Replace("`n", "&#xD;&#xA;").Replace("`t", "&#x09;")
+
+#(gc C:\laps\LAPSSubscription.xml).replace('CHANGETHIS', $xpathlaps) | sc C:\laps\LAPSSubscription.xml -Encoding UTF8
+
+#Setting WinRM Service to automatic start and running quickconfig
+Set-Service -Name winrm -StartupType Automatic
+winrm quickconfig -quiet
+
+#Set the size of the forwarded events log to 500MB
+wevtutil sl forwardedevents /ms:500000000
+
+
+#Running quickconfig for subscription service
+wecutil qc -quiet
+
+#Creating Applocker Subscription from XML files FYI we do delete any existing ones and recreate
+If ((wecutil gs "LAPS Audit Events") -ne $NULL) {
+    wecutil ds "LAPS Audit Events"
+    wecutil cs .\LAPSSubscription.xml
+    }
+Else {wecutil cs .\LAPSSubscription.xml} 
+
+#Fix up the query in the registry with the proper text with carriage returns replaced.. carriage returns in the xml file imported via wecutil do not make it to the registry key
+$xpathguid = $xpathguid.Replace("`n","`r`n")
+$xpathlaps = "<QueryList><Query Id=""0"" Path=""Security""><Select Path=""Security"">*[System[EventID=4662]] and *[EventData[Data[@Name='SubjectUserSid'] !='S-1-5-18']] and *[EventData[Data[@Name='ObjectType'] and (Data='%{bf967a86-0de6-11d0-a285-00aa003049e2}')]] and *[EventData[Data[@Name='Properties'] and $xpathguid]]</Select></Query></QueryList>"
+Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\EventCollector\Subscriptions\LAPS Audit Events" -Name "Query" -Value $xpathlaps
+wecutil rs "LAPS Audit Events"
+
+
+#FYI if you need to export Subscriptions to fix SIDS or anything in an environment 
+#use wecutil gs "%subscriptionname%" /f:xml >>"C:\Temp\%subscriptionname%.xml"
+
+#Creating Task Scheduler Item to restart parsing script on reboot of system.
+If ((Get-ScheduledTask -TaskName "LAPS Parsing Task") -ne $NULL) {
+    Unregister-ScheduledTask -TaskName "LAPS Parsing Task" -Confir:$false
+    schtasks.exe /create /tn "LAPS Parsing Task" /xml LAPSParsingTask.xml
+    }
+Else {schtasks.exe /create /tn "LAPS Parsing Task" /xml LAPSParsingTask.xml}
